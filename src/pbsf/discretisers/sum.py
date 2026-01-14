a3849839003de9472b51a3c6455483ceec31b97b
Patrick Van der Spiegel <patrick@vanderspie.gl>
2026-01-14 15:12:34 +0100

    Implement Sum discretiser + basic SumNode tests

---
+++ src/pbsf/discretisers/sum.py
@@ -0,0 +1,76 @@
+from collections.abc import Callable
+from typing import Any
+
+import numpy as np
+
+from pbsf.nodes import SumNode
+from pbsf.utils import has_required
+from pbsf.discretisers.base import Discretiser, _divide
+
+class Summation(Discretiser):
+    """
+    Discretise contiguous subsequences by summing the values of an increasing
+    number of frames.
+
+    This is a toy Discretiser implementation example for demonstration purposes.
+
+    Parameters
+    ----------
+    params : dict[str, Any] | None, default=None
+        Configuration dictionary with the following required keys:
+
+        - max_depth (Callable): Function that returns the maximum depth of the
+          chain given the data.
+        - frames (Callable): Function that returns the number of frames for a
+          given depth.
+        - node_type (type): The type of the node to use for the chain. Must be
+          SumNode.
+        - node_params (dict): The parameters to pass to the node constructor.
+    """
+    def __init__(self, params: dict[str, Any] | None = None) -> None:
+        has_required(params, [
+            ("max_depth", Callable),
+            ("frames", Callable),
+            ("node_type", [SumNode]),
+            ("node_params", dict)
+        ])
+        self.max_depth = params["max_depth"]
+        self.frames = params["frames"]
+        self.node_type = params["node_type"]
+        self.node_params = params["node_params"]
+
+    def discretise(self, segment: np.ndarray) -> list:
+        """
+        Discretise a segment by splitting it in a number of frames and summing
+        the frame values.
+
+        Parameters
+        ----------
+        segment : np.ndarray
+            The segment to discretise.
+
+        Returns
+        -------
+        list
+            A list of nodes representing the segment in increasing granularity,
+            forming a chain from coarse to fine-grained approximations.
+
+        Raises
+        ------
+        ValueError
+            If the segment is not 1D.
+        """
+        nodes = []
+        if segment.ndim != 1:
+            raise ValueError(f"Can only discretise 1D data.")
+        for depth in range(self.max_depth(segment)):
+            breakpoints = _divide(0, len(segment), self.frames(depth))
+            sums = np.array([np.sum(segment[i:j]) for (i, j) in breakpoints])
+            nodes.append(
+                self.node_type({
+                    "depth": depth,
+                    "sums": sums,
+                    **self.node_params
+                })
+            )
+        return nodes
---
+++ tests/nodes/test_sum_node.py
@@ -0,0 +1,53 @@
+import unittest
+import numpy as np
+
+from pbsf.nodes import SumNode
+
+
+class TestSumNode(unittest.TestCase):
+    def test_distance(self):
+        """Test the distance calculation between SumNode instances."""
+        n1 = SumNode({
+            "depth": 0,
+            "sums": np.array([100, 200, 300]),
+            "distance_threshold": lambda depth: 10.0
+        })
+
+        n2 = SumNode({
+            "depth": 0,
+            "sums": np.array([115, 195, 300]),
+            "distance_threshold": lambda depth: 10.0
+        })
+
+        self.assertEqual(n1.distance(n1), 0.0)
+        self.assertEqual(n2.distance(n2), 0.0)
+        self.assertAlmostEqual(n1.distance(n2), 20/3)
+        self.assertAlmostEqual(n2.distance(n1), 20/3)
+        
+        n2.sums = np.array([300, 195, 115])
+        self.assertEqual(n1.distance(n2), 130.0)
+        self.assertEqual(n1.distance(n2), 130.0)
+
+    def test_equality(self):
+        """Test the equivalence of SumNode instances."""
+        n1 = SumNode({
+            "depth": 0,
+            "sums": np.array([100, 200, 300]),
+            "distance_threshold": lambda depth: 10.0
+        })
+
+        n2 = SumNode({
+            "depth": 0,
+            "sums": np.array([115, 195, 300]),
+            "distance_threshold": lambda depth: 10.0
+        })
+
+        self.assertEqual(n1, n2)
+        self.assertEqual(n2, n1)
+
+        threshold = 0.1
+        n1.distance_threshold = threshold
+        n2.distance_threshold = threshold
+
+        self.assertNotEqual(n1, n2)
+        self.assertNotEqual(n2, n1)
