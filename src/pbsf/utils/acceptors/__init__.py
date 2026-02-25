42de098bc96160e5a333bacd360efc6795f91f5d
Patrick Van der Spiegel <patrick@vanderspie.gl>
2026-02-25 14:15:11 +0100

    Export biDFA from acceptors package

--- src/pbsf/utils/acceptors/__init__.py
+++ src/pbsf/utils/acceptors/__init__.py
@@ -1,6 +1,7 @@
 """Finite acceptor implementations for recognising formal languages."""
 
 from .acceptors import FiniteAcceptor
+from .bidfa import biDFA
 from .dfa import DFA
 
-__all__ = ["FiniteAcceptor", "DFA"]
+__all__ = ["FiniteAcceptor", "DFA", "biDFA"]
