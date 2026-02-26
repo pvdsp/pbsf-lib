d4af94f289e9eb5be8e6e95c73429057cf67d023
Patrick Van der Spiegel <patrick@vanderspie.gl>
2026-02-26 17:15:52 +0100

    Add Word class

---
+++ src/pbsf/utils/words/word.py
@@ -0,0 +1,53 @@
+"""Data structure related to a word."""
+
+from typing import Any, Iterable
+
+
+class Word:
+    """
+    Immutable finite sequence of symbols.
+
+    If no iterable is provided, the empty word is created.
+    If an iterable is provided, its items form the symbols of the word.
+    Words are used as input for FiniteAcceptors.
+    """
+
+    def __init__(self, sequence: Iterable[Any] | None):
+        self.sequence = tuple(sequence) if sequence is not None else ()
+
+    def __len__(self):
+        """Return number of symbols in the word."""
+        return len(self.sequence)
+
+    def __iter__(self):
+        """Iterate over the symbols of the word."""
+        return iter(self.sequence)
+
+    def __eq__(self, other: Any) -> bool:
+        """Check if the word is equal to another object."""
+        if not isinstance(other, Word):
+            return False
+        return self.sequence == other.sequence
+
+    def __hash__(self):
+        """Return the hash of the word."""
+        return hash(self.sequence)
+
+    def __getitem__(self, key: int | slice) -> Any:
+        """Return the symbol at a position, or get a subword from a slice."""
+        if isinstance(key, slice):
+            return Word(self.sequence[key])
+        elif isinstance(key, int):
+            return self.sequence[key]
+
+    def __repr__(self) -> str:
+        """Return string representation of the word."""
+        return f"Word({self.sequence})"
+
+    def __add__(self, other: 'Word') -> 'Word':
+        """Return the concatenation of two words."""
+        return Word(self.sequence + other.sequence)
+
+    def __mul__(self, n: int) -> 'Word':
+        """Return the concatenation of the word with n times itself."""
+        return Word(self.sequence * n)
